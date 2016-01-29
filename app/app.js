bffef63b7af802b4e6b684459e73c1b6f746788c
jlumanso <jerissa_lumansoc@optum.com>
2016-01-29 11:41:21 -0800

    update transactions service

--- app/app.js
+++ app/app.js
@@ -42,6 +42,230 @@ app.service('transactionsService', ['$http', '$q', '$rootScope', function($http,
     };
 }]);
 
+app.service('transactionsSrv', ['$http', '$q', '$filter', '$rootScope',
+    function($http, $q, $filter, $rootScope) {
+
+        // Static Data
+        var categories = [];
+        this.getCategories = function() {
+            return categories;
+        };
+
+        var accounts = [];
+        this.getAccounts = function() {
+            return accounts;
+        };
+
+        var status = [];
+        this.getStatus = function() {
+            return status;
+        };
+
+        function initStaticData() {
+            var defer = $q.defer();
+            var promises  = [];
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/categories'
+            }).then(function successCallback(response) {
+                promises.push(categories = response.data);
+            });
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/status'
+            }).then(function successCallback(response) {
+                promises.push(status = response.data);
+            });
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/accounts'
+            }).then(function successCallback(response) {
+                promises.push(accounts = response.data);
+            });
+
+
+            $q.all(promises).then(function() {
+                defer.resolve($rootScope.$broadcast('transactions:init', true));
+            });
+
+            return defer.promise;
+        }
+        initStaticData();
+
+        // Account Balances
+        var balances = {
+            overall: 0,
+            debit: 0,
+            credit: 0
+        };
+        this.getBalances = function() {
+            return balances;
+        };
+
+        // Searching Transactions
+        this.searchTransactions = function(filter) {
+            return $filter('filterSearchTransaction')(allTransactions, filter);
+        };
+
+        // Transactions Interactions
+        var allTransactions = [];
+
+        this.addTransaction = function(transaction) {
+            var defer = $q.defer();
+
+            $http({
+                method: 'POST',
+                url: 'http://localhost:3000/transactions',
+                data: transaction
+            }).then(function successCallback(response) {
+                updateAllTransactions().then(function() {
+                    defer.resolve();
+                });
+            });
+
+            return defer.promise;
+        };
+
+        this.addTransactions = function(transactions) {
+            var defer = $q.defer();
+            var promises  = [];
+
+            angular.forEach(transactions, function(transaction) {
+                $http({
+                    method: 'POST',
+                    url: 'http://localhost:3000/transactions',
+                    data: transaction
+                }).then(function successCallback(response) {
+                    promises.push(response.data);
+                });
+            });
+
+            $q.all(promises).then(function() {
+                updateAllTransactions().then(function() {
+                    defer.resolve();
+                });
+            });
+
+            return defer.promise;
+        };
+
+        this.deleteTransaction = function(id) {
+            var defer = $q.defer();
+
+            $http({
+                method: 'DELETE',
+                url: 'http://localhost:3000/transactions/' + id
+            }).then(function successCallback(response) {
+                updateAllTransactions().then(function() {
+                    defer.resolve();
+                });
+            });
+
+            return defer.promise;
+        };
+
+        this.editTransactions = function(transaction) {
+            var defer = $q.defer();
+
+            $http({
+                method: 'PUT',
+                url: 'http://localhost:3000/transactions/' + transaction.id,
+                data: transaction
+            }).then(function successCallback(response) {
+                updateAllTransactions().then(function() {
+                    defer.resolve();
+                });
+            });
+
+            return defer.promise;
+        };
+
+        var filteredCreditTransactions = [];
+        this.getCreditTransactions = function() {
+            return filteredCreditTransactions;
+        };
+
+        var draftTransactions = [];
+        this.getDraftTransactions = function() {
+            return draftTransactions;
+        };
+
+        this.getTransactions = function() {
+            return allTransactions;
+        };
+
+        function updateAllTransactions() {
+            var defer = $q.defer();
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:3000/transactions?_sort=date&_order=DESC'
+            }).then(function successCallback(response) {
+                allTransactions = response.data;
+                analyzeAllTransactions().then(function() {
+                    defer.resolve();
+                });
+            });
+
+            return defer.promise;
+        }
+
+        function analyzeAllTransactions() {
+            var defer = $q.defer();
+
+            filteredCreditTransactions = [];
+            draftTransactions = [];
+
+            var categoryState;
+            var totalBalance = 0;
+            var debitBalance = 0;
+            var creditBalance = 0;
+
+            for(var i = allTransactions.length - 1; i >= 0; i--) {
+                var transaction = allTransactions[i];
+
+                // category state
+                if(categories[transaction.categoryId]
+                    && categories[transaction.categoryId].parentId === "Expense") {
+                    categoryState = -1;
+                } else {
+                    categoryState = 1;
+                }
+
+                // balances
+                transaction.balance = totalBalance + categoryState * transaction.amount;
+                totalBalance = transaction.balance;
+
+                // debit balance
+                if(transaction.accountId === 0) {
+                    debitBalance += categoryState * transaction.amount;
+                }
+
+                // credit pending/unpaid balance
+                if(transaction.accountId === 1 && (transaction.statusId === 1 || transaction.statusId === 3)) {
+                    creditBalance += transaction.amount;
+                    filteredCreditTransactions.push(transaction);
+                }
+
+                // draft transaction
+                if(transaction.statusId === 0) {
+                    draftTransactions.push(transaction);
+                }
+            }
+
+            balances.debit = debitBalance;
+            balances.credit = creditBalance;
+            balances.overall = debitBalance + creditBalance;
+
+            defer.resolve();
+
+            return defer.promise;
+        }
+    }]);
+
 app.filter('filterSearchTransaction', ['$filter', '$q', function($filter) {
     return function(input, filterOptions) {
         var output = [];
@@ -66,39 +290,6 @@ app.filter('filterSearchTransaction', ['$filter', '$q', function($filter) {
         if(filterOptions.description !== '') {
             output = $filter('filter')(output, filterOptions.description);
         }
-
-        return output;
-    }
-}]);
-
-app.filter('filterCreditTransactions', ['$filter', '$q', function($filter) {
-    return function(input, filterOptions) {
-        var output = [];
-
-        angular.forEach(input, function(transaction) {
-            var validAccountId = transaction.accountId === 1;
-            var validStatusId = (transaction.statusId === 1 || transaction.statusId === 3);
-
-            if(validAccountId && validStatusId) {
-                output.push(transaction);
-            }
-        });
-
-
-        return output;
-    }
-}]);
-
-app.filter('filterDraftTransactions', ['$filter', '$q', function($filter) {
-    return function(input, filterOptions) {
-        var output = [];
-
-        angular.forEach(input, function(transaction) {
-            if(transaction.statusId === 0) {
-                output.push(transaction);
-            }
-        });
-
 
         return output;
     }
